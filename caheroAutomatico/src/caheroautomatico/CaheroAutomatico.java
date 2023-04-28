1f83ec708813f81a7ac4508a56efd872305e2682
Edgar David Caamal <edcaamal@uacam.mx>
2023-04-28 16:22:22 -0500

    ingresarSaldo

--- caheroAutomatico/src/caheroautomatico/CaheroAutomatico.java
+++ caheroAutomatico/src/caheroautomatico/CaheroAutomatico.java
@@ -2,14 +2,19 @@ package caheroautomatico;
 
 import models.Cliente;
 import static models.Cliente.clientes;
+import static models.Cliente.ingresarSaldo;
 
 public class CaheroAutomatico {
 
     public static void main(String[] args) {
         
-        clientes.add(new Cliente("Ignacio Lopez", "7777777", 0.0));
-        clientes.add(new Cliente("Javier Lopez", "8888888", 0.0));
+        clientes.add(new Cliente("Ignacio Lopez", "7777777", 0.0)); // 0
+        clientes.add(new Cliente("Javier Lopez", "8888888", 0.0));  // 1
+        System.out.println(clientes);
         
+        ingresarSaldo(1, 100.00);
+        System.out.println(clientes);
+        ingresarSaldo(1, 70.00);
         System.out.println(clientes);
         
     }
